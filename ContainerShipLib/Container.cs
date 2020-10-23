ef2a06edc9aa9971eda666d92b8aa7f72dec1cce
leontienhelderman <l.helderman@hotmail.com>
2020-10-23 15:38:52 +0200

    Initial commit

---
+++ ContainerShipLib/Container.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ContainerShipLib
+{
+    public class Container
+    {
+        public type ContainerType { get; private set; }
+        public weight ContainerWeight { get; private set; }
+
+        public Container(type type, weight weight)
+        {
+            ContainerType = type;
+            ContainerWeight = weight;
+        }
+
+        public enum type
+        {
+            normal,
+            valuable,
+            coolable,
+            valuecool
+        }
+
+        public enum weight
+        {
+            empty = 4,
+            full = 3000
+        }
+    }
+}
