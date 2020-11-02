fd045e8e16947fb3a889a543361336ec2ca2ff47
leontienhelderman <l.helderman@hotmail.com>
2020-11-02 14:00:19 +0100

    Stack weight check works

---
+++ ContainerShipLib/Stack.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace ContainerShipLib
+{
+    public class Stack
+    {
+        private List<Container> stack;
+
+        private int WeightCapacity = 120000;
+        public Stack()
+        {
+            stack = new List<Container>();
+
+        }
+
+        public bool AddContainerToStack(Container _container)
+        {
+            if (!StackWeightFit(_container))
+            {
+                return false;
+            }
+
+            stack.Add(_container);
+            stack = OrderStackByWeight(stack);
+            
+
+            return true;
+
+        }
+
+        private bool StackWeightFit(Container container)
+        {
+            List<Container> tempList = new List<Container>();
+            tempList.AddRange(stack);
+            tempList.Add(container);
+            tempList = OrderStackByWeight(tempList);
+
+            var bottomContainer = tempList.Remove(tempList.Last());
+
+            var tempList2 = tempList;
+
+            int sum = 0;
+            foreach (Container container1 in tempList2)
+            {
+                sum += container1.ContainerWeight;
+            }
+
+            if (sum <= WeightCapacity)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void OrderByContainerType(List<Container> containers)
+        {
+            List<Container> coolablecontainers = new List<Container>();
+            List<Container> valuablecontainers = new List<Container>();
+            List<Container> valuecoolcontainers = new List<Container>();
+            List<Container> normalcontainers = new List<Container>();
+
+            foreach (Container container in containers)
+            {
+                if (container.ContainerType == Container.type.coolable)
+                {
+                    coolablecontainers.Add(container);
+                }
+                else if (container.ContainerType == Container.type.valuable)
+                {
+                    valuablecontainers.Add(container);
+                }
+                else if (container.ContainerType == Container.type.valuecool)
+                {
+                    valuecoolcontainers.Add(container);
+                }
+                else if (container.ContainerType == Container.type.normal)
+                {
+                    normalcontainers.Add(container);
+                }
+            }
+
+            coolablecontainers = OrderStackByWeight(coolablecontainers);
+            valuablecontainers = OrderStackByWeight(valuablecontainers);
+            valuecoolcontainers = OrderStackByWeight(valuecoolcontainers);
+            normalcontainers = OrderStackByWeight(normalcontainers);
+        }
+
+        public List<Container> OrderStackByWeight(List<Container> containers)
+        {
+            return containers.OrderBy(c => c.ContainerWeight).ToList();
+        }
+
+    }
+}
---
+++ ContainerShipTest/StackTest.cs
@@ -0,0 +1,46 @@
+﻿using ContainerShipLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContainerShipTest
+{
+    public class StackTest
+    {
+        private Stack stack;
+
+        [SetUp]
+        public void SetUp()
+        {
+            stack = new Stack();
+            stack.AddContainerToStack(new Container(Container.type.normal, 25000));
+            stack.AddContainerToStack(new Container(Container.type.normal, 30000));
+            stack.AddContainerToStack(new Container(Container.type.normal, 20000));
+            stack.AddContainerToStack(new Container(Container.type.normal, 15000));
+            stack.AddContainerToStack(new Container(Container.type.normal, 26000));
+
+            //add this container so the sum of the other containers weight will be 116000 this container will be removed from the list to do the sum
+            stack.AddContainerToStack(new Container(Container.type.normal, 30000));
+        }
+
+        [Test]
+        public void AddContainerToStack_ReturnsTrue()
+        {
+            var stack = new Stack();
+            Assert.IsTrue(stack.AddContainerToStack(new Container(Container.type.normal, 15300)));
+        }
+
+        [Test]
+        public void AddContainerToStack_ExceedStackWeightCapacity_returnsFalse()
+        {
+            Assert.IsFalse(stack.AddContainerToStack(new Container(Container.type.normal, 4001)));
+        }
+
+        [Test]
+        public void AddContainerToStack_FitsPerfectly_returnsTrue()
+        {
+            Assert.IsTrue(stack.AddContainerToStack(new Container(Container.type.normal, 4000)));
+        }
+    }
+}
